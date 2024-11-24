d13536218aae50846525dd82a9aec474d2d4d741
Tony <akruk@kse.org.ua>
2024-11-24 21:57:48 +0200

    upd

--- deployment/config.sh
+++ deployment/config.sh
@@ -18,6 +18,12 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 source <(minikube completion bash)
 
+# Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
+
 # Start
 minikube start
 
